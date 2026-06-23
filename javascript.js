713c090f44acb8275b0176a3f7f52bf1c735b4d5
shaheeriqbal341 <develdawel350@gmail.com>
2026-06-24 00:24:43 +0500

    First commit

---
+++ javascript.js
@@ -0,0 +1,28 @@
+let display = document.getElementById('display');
+
+function append(value) {
+    if(display.value === "0"){
+        display.value = value;
+    }else{
+        display.value += value;
+    }
+}
+
+function clearDisplay() {
+    display.value = "0";
+}
+
+function backspace() {
+    display.value = display.value.slice(0,-1);
+    if(display.value === ""){
+        display.value = "0";
+    }
+}
+
+function calculate() {
+    try{
+        display.value = eval(display.value);
+    } catch(error){
+        display.value = "Error";
+    }
+}
